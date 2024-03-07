814e07f84dca9b390ee538253f78fc7f0fc9dcc8
Feven <feventsegay6@gmail.com>
2024-03-07 12:51:27 -0600

    added components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,19 @@
-import { Button } from '@nextui-org/react';
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
+import { Divider } from '@nextui-org/react';
+import TopicCreateForm from '@/components/topics/topic-create-form';
+import TopicList from '@/components/topics/topic-list';
 
-export default async function Home() {
-  const session = await auth();
-
+export default function Home() {
   return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-
-      <form action={actions.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-
-      {session?.user ? (
-        <div>{JSON.stringify(session.user)}</div>
-      ) : (
-        <div>Signed Out</div>
-      )}
-
-      <Profile />
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div className="border shadow py-3 px-2">
+        <TopicCreateForm />
+        <Divider className="my-2" />
+        <h3 className="text-lg">Topics</h3>
+        <TopicList />
+      </div>
     </div>
   );
 }
---
+++ src/components/common/form.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useFormState } from 'react-dom'
+import { Input, Button, Textarea, Popover, PopoverTrigger, PopoverContent } from '@nextui-org/react'
+import * as actions from '@/actions'
+
+interface FormProps {
+  buttonName: string
+  title: string
+}
+
+export default function Form({ buttonName, title }: FormProps) {
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {}
+  })
+
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">{buttonName}</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={action}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">{title}</h3>
+            <Input name="name" label="Name" labelPlacement="outside" placeholder="Name" isInvalid={!!formState.errors.name} errorMessage={formState.errors.name?.join(', ')} />
+
+            <Textarea name="description" label="Description" labelPlacement="outside" placeholder="Describe your topic" isInvalid={!!formState.errors.description} errorMessage={formState.errors.description?.join(', ')} />
+
+            {formState.errors._form ? <div className="rounded p-2 bg-red-200 border border-red-400">{formState.errors._form?.join(', ')}</div> : null}
+
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  )
+}
